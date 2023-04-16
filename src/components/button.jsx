8711da6a44a48bc3c707708dda22d6c859524c7d
Raj <raj2356.be21@chitkara.edu.in>
2023-04-16 14:37:36 +0530

    Add event Listener to button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {userIsRegistered} from "./App"
 function Button(prop) {
+  const [touch, touchFunction]= useState(false);
+  function onOver(){
+    touchFunction(true);
+    console.log("Over");  // it will not show when console is open. 
+  }
+
+  function onOut(){
+    touchFunction(false);
+  }
+
   let display = "";
   userIsRegistered ? (display = "Login") : (display = "Register");
-  return <button type={prop.type}>{display}</button>;
+  return <button style={{backgroundColor: touch ? "black" : "white"}} onMouseOver={onOver} onMouseOut={onOut} type={prop.type}>{display}</button>;
 }
 
 export default Button;
